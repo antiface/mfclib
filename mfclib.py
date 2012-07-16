3712ba0c05561f9317c01c0aacd43d505d2dbcaa
Manoel Carvalho <manoelhc@gmail.com>
2012-07-15 21:14:42 -0300

    Added asserts as unit test

--- mfclib.py
+++ mfclib.py
@@ -33,21 +33,21 @@ class mfclib:
 			self._create_structure()
 
 	def _has_db(self):
-		self._db_exec('select count(*) "total" from sqlite_master where type like "table" and name like "fc_words"')
+		self._db_exec('select count(*) "total" from sqlite_master where type like "table" and name like "fc_terms"')
 		if self._db_one()[0] == 0:
 			return False
 		return True
 
 	def _create_structure(self):
-		self._db_exec("create table fc_words (word_id INTEGER PRIMARY KEY AUTOINCREMENT, word TEXT);")
+		self._db_exec("create table fc_terms (term_id INTEGER PRIMARY KEY AUTOINCREMENT, term TEXT);")
 		self._db_exec("create table fc_tags (tag_id INTEGER PRIMARY KEY AUTOINCREMENT, tag TEXT);")
-		self._db_exec("create table fc_taggeds (tag_id INTEGER, word_id INTEGER)")
+		self._db_exec("create table fc_taggeds (tag_id INTEGER, term_id INTEGER)")
 		self._db_exec("create table fc_phrases (phrase_id INTEGER PRIMARY KEY AUTOINCREMENT);")
-		self._db_exec("create table fc_rel (tag_id INTEGER, word_id1 INTEGER, word_id2 INTEGER);")
-		self._db_exec("create table fc_sym (tag_id1 INTEGER, word_id1 INTEGER, tag_id2 INTEGER, word_id2 INTEGER);")
-		self._db_exec("create table fc_links (tag_id INTEGER, word_id INTEGER, phrase_id INTEGER);")
-		self._db_exec("create table fc_review (word_id INTEGER, date INTEGER);")
-		self._db_exec("create table fc_status (word_id INTEGER, corrects INTEGER, errors INTEGER, karma INTEGER);")
+		self._db_exec("create table fc_rel (tag_id INTEGER, term_id1 INTEGER, term_id2 INTEGER);")
+		self._db_exec("create table fc_sym (tag_id1 INTEGER, term_id1 INTEGER, tag_id2 INTEGER, term_id2 INTEGER);")
+		self._db_exec("create table fc_links (tag_id INTEGER, term_id INTEGER, phrase_id INTEGER);")
+		self._db_exec("create table fc_review (term_id INTEGER, date INTEGER);")
+		self._db_exec("create table fc_status (term_id INTEGER, corrects INTEGER, errors INTEGER, karma INTEGER);")
 		
 		return None
 
@@ -55,71 +55,104 @@ class mfclib:
 	def add_tag(self, name):
 		data = {'tag' : name}
 
-		self._db_exec("insert into fc_tags (tag) values (:tag)", data)
-		self._set_cache('langs', None)
 		self._db_exec("select tag_id from fc_tags where tag = :tag", data)
-		return self._set_cache('word', self._db_one()[0])
+		if self._db_one() == None:
+			self._db_exec("insert into fc_tags (tag) values (:tag)", data)
+			self._db_exec("select tag_id from fc_tags where tag = :tag", data)
+			return self._db_one()[0]
+		
+		return None
 
 	def edit_tag(self, tag_id, name):
-		self._set_cache('tags', None)
 		self._db_exec("update fc_tags set tag = :tag where tag_id = :tag_id", {'tag':name, 'tag_id':tag_id})
 
-
 	def get_tags(self):
-		cache = self._get_cache('tags')
-		
-		if cache == None:
-			self._db_exec("select * from fc_tags")
-			cache = self._db_fetch_all()
-			self._set_cache('tags', cache)
-
+		self._db_exec("select * from fc_tags")
+		cache = self._db_fetch_all()
 		return cache
 
-	def add_word(self, word):
-		self._db_exec("select count(*) total from fc_words where word like :word", {'word' : word})
-		if self._db_one()[0] == 0:
-			self._db_exec("insert into fc_words (word) values (:word)", {'word' : word})
+	def get_tag_id(self, tag):
 		
-		self._db_exec("select word_id from fc_words where word like :word", {'word' : word})
+		data = {'tag': tag}
+
+		self._db_exec("select tag_id from fc_tags where tag like :tag", data)
+		res = self._db_one()
+		
+		if res != None:
+			return res[0]
+		return None
+
+	def get_tag_by_id(self, tag_id):
+		
+		data = {'tag_id': tag_id}
+
+		self._db_exec("select tag from fc_tags where tag_id = :tag_id", data)
+		res = self._db_one()
+		
+		if res != None:
+			return res[0]
+		return None
+
+
+
+	def add_term(self, term):
+		self._db_exec("select count(*) total from fc_terms where term like :term", {'term' : term})
+		if self._db_one()[0] == 0:
+			self._db_exec("insert into fc_terms (term) values (:term)", {'term' : term})
+		
+		self._db_exec("select term_id from fc_terms where term like :term", {'term' : term})
 		return self._db_one()[0]
 
-	def tag(self, word_id, tag_id):
-		data = {'word_id' : word_id, 'tag_id' : tag_id}
-		self._db_exec("select count(*) total from fc_taggeds where word_id = :word_id and tag_id = :tag_id", data)
-		if self._db_one()[0] == 0:
-			self._db_exec("insert into fc_taggeds (word_id, tag_id) values (:word_id, :tag_id)", data)
+	def tag(self, term_id, tag_id):
+		data = {'term_id' : term_id, 'tag_id' : tag_id}
+		self._db_exec("select count(*) total from fc_taggeds where term_id = :term_id and tag_id = :tag_id", data)
+		if self._db_one() == None:
+			self._db_exec("insert into fc_taggeds (term_id, tag_id) values (:term_id, :tag_id)", data)
 			return True
 		return False
 
 		
 
 
-	def get_words_by_tag(self, tag_id):
+	def get_terms_by_tag(self, tag_id):
 		
-		self._db_exec("select words.word_id, words.word from fc_tagged, words where fc_tagged.tag_id = :tag_id", {'tag_id' : tag_id})
+		self._db_exec("select terms.term_id, terms.term from fc_tagged, terms where fc_tagged.tag_id = :tag_id", {'tag_id' : tag_id})
 		return self._db_fetch_all()	
 	
 
 #UnitTest		
-
-test = mfclib('test.data')
-
-test.add_tag('English')
-
-tags = test.get_tags()
-print tags
-
-word_id = test.add_word("Love")
+if __name__ == '__main__':
 
 
-print test.tag(word_id, tags[0][0])
-print test.tag(word_id, tags[0][0])
+	db_name = '/tmp/.mfclib_unittest.data'
+	test = mfclib(db_name)
+	#add tag test
+	test.add_tag('English')
+	tags = test.get_tags()
+
+	assert str(tags) == "[(1, u'English')]", 'Add tag test FAILED!'
+
+	test.add_tag('English')
+	tags = test.get_tags()
+
+	assert str(tags) == "[(1, u'English')]", 'Add duplicated tag FAILED!'
+
+	test.add_tag('French')
+	tags = test.get_tags()
+	
+	assert str(tags) == "[(1, u'English'), (2, u'French')]", 'Add second tag test FAILED!'
+	assert 1 == test.get_tag_id('English'), 'Add get tag_id FAILED!'
+	assert 1 == test.get_tag_id('englisH'), 'Add get tag_id FAILED!'
+	assert 2 == test.get_tag_id('FrEnCh'), 'Add get tag_id FAILED!'
+	assert None == test.get_tag_id('German'), 'Add get tag_id FAILED!'
+
+	assert 'English' == test.get_tag_by_id(1), 'Add get tag_by_id FAILED!'
+	assert 'French' == test.get_tag_by_id(2), 'Add get tag_by_id FAILED!'
+	assert None == test.get_tag_by_id(3), 'Add get tag_id FAILED!'
 
 
+	print 'Congrats! Test passed!'
 
 
-
-
-
-os.system('rm test.data')
+	os.system('rm ' + db_name)
 
